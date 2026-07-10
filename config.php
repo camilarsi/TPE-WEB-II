a74cd0595575bee2648a2408fa01564ab09caa65
Camila Silva <camilarsilva@hotmail.com.ar>
2026-07-10 15:35:57 -0300

    calcular BASE_URL dinamicamente con $_SERVER

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ define('DB_HOST',   'localhost');
 define('DB_NAME',   'db_isolde_maren');
 define('DB_USER',   'root');
 define('DB_PASS',   '');
-define('BASE_URL', '/TPE-WEB-II/');
+define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']) . '/');
 define('JWT_KEY', 'IsoldeM@ren_2026#Esculturas$JWT!xK9p');
 define('JWT_EXP', 3600);
