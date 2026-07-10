e4365fa1947086057ad95ae557d47df3335d64f7
Camila Silva <camilarsilva@hotmail.com.ar>
2026-07-10 17:11:52 -0300

    sacar addPieza/removePieza sin usar del controller publico

--- app/controllers/pieza.controller.php
+++ app/controllers/pieza.controller.php
@@ -2,7 +2,6 @@
 require_once './app/models/pieza.model.php';
 require_once './app/views/pieza.view.php';
 require_once './app/models/serie.model.php';
-require_once './app/helpers/auth.helper.php';
 
 class PiezaController
 {
@@ -12,9 +11,6 @@ class PiezaController
 
     public function __construct()
     {
-        // verifico logueado
-        //AuthHelper::verify(); implementar para admin
-
         $this->model = new PiezaModel();
         $this->view = new PiezaView();
         $this->serieModel = new SerieModel();
@@ -29,36 +25,6 @@ class PiezaController
         $piezas = $this->model->getPiezas($page, $limit);
         $series = $this->serieModel->getSeries();
         $this->view->showPiezas($piezas, $series, $page, $pages);
-    }
-
-    public function addPieza()
-    {
-
-        // obtengo los datos del usuario
-        $title = $_POST['title'];
-        $description = $_POST['description'];
-        $materiales = $_POST['materiales'];
-        $id_serie = $_POST['id_serie'];
-
-
-        // validaciones
-        if (empty($title) || empty($description) || empty($materiales) || empty($id_serie)) {
-            $this->view->showError("Debe completar todos los campos");
-            return;
-        }
-
-        $id = $this->model->insertarPieza($title, $description, $materiales, $id_serie);
-        if ($id) {
-            header('Location: ' . BASE_URL);
-        } else {
-            $this->view->showError("Error al insertar la pieza");
-        }
-    }
-
-    function removePieza($id)
-    {
-        $this->model->deletePieza($id);
-        header('Location: ' . BASE_URL);
     }
 
     public function show($id)
